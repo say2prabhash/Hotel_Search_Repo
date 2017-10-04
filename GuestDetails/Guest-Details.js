dee77a1066e9ed6442d03a02bd23f127185feebc
Bandana Mahapatra <bmahapatra@tavisca.com>
2017-10-04 09:25:23 +0530

    guest detail page

---
+++ GuestDetails/Guest-Details.js
@@ -0,0 +1,47 @@
+
+function month()
+{
+for(y = 2000; y <= 2500; y++) {
+        var optn = document.createElement("OPTION");
+        optn.text = y;
+        optn.value = y;
+        
+        // if year is 2015 selected
+        if (y == 2015) {
+            optn.selected = true;
+        }
+        
+        document.getElementById('year').options.add(optn);
+}
+
+
+function year()
+{
+var d = new Date();
+var monthArray = new Array();
+monthArray[0] = "January";
+monthArray[1] = "February";
+monthArray[2] = "March";
+monthArray[3] = "April";
+monthArray[4] = "May";
+monthArray[5] = "June";
+monthArray[6] = "July";
+monthArray[7] = "August";
+monthArray[8] = "September";
+monthArray[9] = "October";
+monthArray[10] = "November";
+monthArray[11] = "December";
+for(m = 0; m <= 11; m++) {
+    var optn = document.createElement("OPTION");
+    optn.text = monthArray[m];
+    // server side month start from one
+    optn.value = (m+1);
+ 
+    // if june selected
+    if ( m == 5) {
+        optn.selected = true;
+    }
+ 
+    document.getElementById('month').options.add(optn);
+}
+}
