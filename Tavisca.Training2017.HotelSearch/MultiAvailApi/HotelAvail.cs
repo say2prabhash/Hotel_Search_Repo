51b388987aa26e99806cac0ec43666948c0d5a3f
Suraj Narayan <snarayan@tavisca.com>
2017-10-05 10:56:40 +0530

    Added structure for MultiAvail Api

---
+++ Tavisca.Training2017.HotelSearch/MultiAvailApi/HotelAvail.cs
@@ -0,0 +1,13 @@
+﻿using MultiAvail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiAvailApi
+{
+    public class HotelAvail
+    {
+        
+    }
+}
